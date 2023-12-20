c764eb4ef268ceb37fd947fb0a942dcbd647d0b3
Hanna Borodina <venera131916@gmail.com>
2023-12-20 13:39:19 +0200

    functional now workes correctly

--- src/index.js
+++ src/index.js
@@ -2,84 +2,172 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const apiKey = '28572432-d9bfef10324799b9b4a4c6d39';
 let page = 1;
+let totalImages = 0;
+let currentTotalHits = 0;
+let hoorayNotified = false;
 
-form.addEventListener('submit', async (event) => {
+document.addEventListener('DOMContentLoaded', async () => {
+  loadMoreBtn.style.display = 'none';
+
+  form.addEventListener('submit', async event => {
     event.preventDefault();
+   
     const searchQuery = form.elements.searchQuery.value.trim();
-
-    if(searchQuery !== '') {
-        try {
-            const response = await axios.get('https://pixabay.com/api/', {
-                params: {
-                    key: apiKey,
-                    q: searchQuery,
-                    image_type: 'photo',
-                    orientation:'horizontal',
-                    safesearch: true,
-                    per_page: 40,
-                    page: page,
-                },
+    if (searchQuery !== '') {   
+        
+      try {
+        gallery.innerHTML = '';
+        page = 1;
+        const response = await axios.get('https://pixabay.com/api/', {
+          params: {
+            key: apiKey,
+            q: searchQuery,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: 40,
+            page: page,
+          },
         });
 
-        const {data} = response;
+        const { data } = response;
         const images = data.hits;
+        currentTotalHits = data.totalHits;
 
-        if(images.length > 0){
-            if(page == 1) {
-                gallery.innerHTML = '';
-            }
+        if (images.length > 0) {
+          if (page === 1) {
+            hoorayNotified = false;
+          }
 
-            images.forEach((image) => {
-                const card = document.createElement('div');
-                card.classList.add('photo-card');
-                card.innerHTML = `
-                
-                
-                <div class="photo-card gallery__item">
+          
+          images.forEach(image => {
+            const card = document.createElement('div');
+            card.classList.add('photo-card');
+            card.innerHTML = `   
                 <a href="${image.largeImageURL}" class="lightbox">
                 <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
                 <div class="info">
                   <p class="info-item">
-                  <i class="fas fa-heart"></i> + ${image.likes}
+                  
+                  <i class="fas fa-heart"><span class="fa-p">${image.likes}</span></i>
                   </p>
                   <p class="info-item">
-                  <i class="fas fa-eye"></i> ${image.views}
+                  <i class="fas fa-eye"><span class="fa-p">${image.views}</span></i> 
                   </p>
                   <p class="info-item">
-                  <i class="fas fa-comment"></i> ${image.comments}
+                  <i class="fas fa-comment"><span class="fa-p">${image.comments}</span></i> 
                   </p>
                   <p class="info-item">
-                  <i class="fas fa-download"></i> ${image.downloads}
+                  <i class="fas fa-download"><span class="fa-p">${image.downloads}</span></i> 
                   </p>
                 </div>
                  </a>
               </div>
              
                 `;
-                gallery.appendChild(card);       
-            });
+            gallery.appendChild(card);
+          });
 
+          totalImages += images.length;
+
+          if (totalImages >= currentTotalHits) {
+            loadMoreBtn.style.display = 'none';
+          
+              Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+            
+          } else {
+            if (!hoorayNotified) {
+                Notiflix.Notify.success(`Hooray! We found ${currentTotalHits} images.`);
+                hoorayNotified = true; 
+              }
             loadMoreBtn.style.display = 'block';
-
-            page += 1;
+            }
+        
+          page += 1;
         } else {
-            Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+            loadMoreBtn.style.display = 'none';
+          Notiflix.Notify.warning(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
         }
-    } catch (error) {
+      } catch (error) {
         console.error(error);
-        Notiflix.Notify.failure("An error occurred while fetching images. Please try again.");
+        Notiflix.Notify.failure(
+          'An error occurred while fetching images. Please try again.'
+        );
+      }
+    } else {
+      Notiflix.Notify.warning('Please enter a search query.');
     }
-}else {
-    Notiflix.Notify.warning("Please enter a search query.");
-}
-});
+  });
 
-loadMoreBtn.addEventListener('click', () => {
-form.dispatchEvent(new Event('submit'));
+  loadMoreBtn.addEventListener('click', async () => {
+    try {
+      const searchQuery = form.elements.searchQuery.value.trim();
+
+      const response = await axios.get('https://pixabay.com/api/', {
+        params: {
+          key: apiKey,
+          q: searchQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 40,
+          page: page,
+        },
+      });
+
+      const { data } = response;
+      const images = data.hits;
+      currentTotalHits = data.totalHits;
+
+      if (images.length > 0) {
+        images.forEach(image => {
+          const card = document.createElement('div');
+          card.classList.add('photo-card');
+          card.innerHTML = `   
+          <a href="${image.largeImageURL}" class="lightbox">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+            
+            <i class="fas fa-heart"><span class="fa-p">${image.likes}</span></i>
+            </p>
+            <p class="info-item">
+            <i class="fas fa-eye"><span class="fa-p">${image.views}</span></i> 
+            </p>
+            <p class="info-item">
+            <i class="fas fa-comment"><span class="fa-p">${image.comments}</span></i> 
+            </p>
+            <p class="info-item">
+            <i class="fas fa-download"><span class="fa-p">${image.downloads}</span></i> 
+            </p>
+          </div>
+           </a>
+          </div>`;
+          gallery.appendChild(card);
+        });
+
+        totalImages += images.length;
+
+        if (totalImages >= currentTotalHits) {
+          loadMoreBtn.style.display = 'none';
+          Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+        }
+
+        page += 1;
+      } else {
+        loadMoreBtn.style.display = 'none';
+        Notiflix.Notify.warning('No more images to load.');
+      }
+    } catch (error) {
+      console.error(error);
+      Notiflix.Notify.failure('An error occurred while fetching more images. Please try again.');
+    }
+  });
 });
